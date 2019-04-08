04414a2fed34eee442c24df1d6f32096aa5e5da8
Mateus Vinicius dos Passos <mviniciusdospassos@gmail.com>
2019-04-08 15:10:16 -0300

    duplicastring

---
+++ URI TESTES/string/dupplica string.cpp
@@ -0,0 +1,34 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+char* duplica(int tam, char* str){
+	
+	int i,j=0;
+	int n=tam*2;
+	
+	char* resp=(char*) malloc((2*tam+1)*sizeof(char));
+	
+ 	for(i=0;i<n;i++){
+ 		resp[i]=str[j];
+ 		j++;
+ 		if(j==tam){
+ 			j=0;
+		}
+	}
+	
+	resp[i]='\0';
+	
+ 	return resp;	
+	
+}
+int main(){
+	
+	char str[100];
+	scanf("%[^\n]", &str);
+	int tam=strlen(str);
+	char* resp = duplica(tam, str);
+	
+	printf("%s", resp);
+	
+	return 0;
+}
